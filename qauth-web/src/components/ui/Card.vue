7a7951d7999cdfc8207c9493c93960427347143f
1222hxy <yhkun666@gmail.com>
2026-06-08 19:30:28 +0800

    Revert "gogogo"

--- qauth-web/src/components/ui/Card.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import { computed } from "vue";
-import { cn } from "../../lib/utils";
-const props = defineProps<{ class?: string }>();
-const classes = computed(() => cn("rounded-2xl border border-black/10 bg-white shadow-sm dark:border-white/10 dark:bg-[#171717]", props.class));
-</script>
-<template><section :class="classes"><slot /></section></template>
